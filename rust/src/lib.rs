15facc3300e6b895d075ac51de35a711440d8e84
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-01-14 16:47:54 +0900

    fix: remove unused code

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,34 +7,3 @@ pub mod leetcode {
     pub mod remove_element;
     pub mod two_sum_two;
 }
-
-// https://leetcode.com/problems/optimal-partition-of-string/?envType=study-plan-v2&envId=amazon-spring-23-high-frequency
-pub fn partition_string(s: String) -> i32 {
-    let mut current_set = HashSet::new();
-
-    let mut count = 0;
-
-    for char in s.bytes() {
-        if current_set.contains(&char) {
-            count = count + 1;
-            current_set = HashSet::new();
-            current_set.insert(char);
-        } else {
-            current_set.insert(char);
-        }
-    }
-
-    count = count + 1;
-
-    return count;
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_partition_string() {
-        assert_eq!(partition_string("abcabc".to_string()), 2);
-    }
-}
