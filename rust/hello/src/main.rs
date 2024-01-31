91749d344a6f8c8b0c05474e4182bd0b70dce8be
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-02-01 00:13:39 +0900

    feat: add 404 to server

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -16,24 +16,25 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
 
-    let http_request: Vec<_> = buf_reader
+    let mut http_request = buf_reader
         .lines()
         .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+        .take_while(|line| !line.is_empty());
 
-    println!("accepted request");
-    for line in http_request {
-        println!("{}", line);
-    }
+    let request_line = http_request.next().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
+    println!("Request: {}", request_line);
 
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(file_name).unwrap();
 
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
     stream.write_all(response.as_bytes()).unwrap();
 }
