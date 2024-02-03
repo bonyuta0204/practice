10eb0c1164125dbd4167e257a0d8c1632f35f238
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-02-03 13:43:46 +0900

    fix: error handling in server

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -7,12 +7,24 @@ use std::{
 
 use hello::ThreadPool;
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = match TcpListener::bind("127.0.0.1:7878") {
+        Ok(listenr) => listenr,
+        Err(e) => {
+            eprintln!("Failed to bind to address: {}", e);
+            return;
+        }
+    };
 
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let stream = match stream {
+            Ok(stream) => stream,
+            Err(e) => {
+                eprintln!("Failed to establish a connection: {}", e);
+                continue;
+            }
+        };
 
         pool.execute(|| {
             handle_connection(stream);
@@ -23,25 +35,31 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
 
-    let mut http_request = buf_reader
+    let http_request: Vec<_> = buf_reader
         .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty());
+        .filter_map(|result| result.ok())
+        .take_while(|line| !line.is_empty())
+        .collect();
 
-    let request_line = http_request.next().unwrap();
-
-    println!("Request: {}", request_line);
-
-    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
+    if let Some(request_line) = http_request.first() {
+        println!("Request: {}", request_line);
+        let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
+            ("HTTP/1.1 200 OK", "hello.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+        match fs::read_to_string(file_name) {
+            Ok(contents) => {
+                let length = contents.len();
+                let response =
+                    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+                stream.write_all(response.as_bytes()).unwrap();
+            }
+            Err(e) => {
+                eprintln!("cannot read from file. {}", e);
+            }
+        };
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+        eprint!("malformated HTTP Request")
     };
-
-    let contents = fs::read_to_string(file_name).unwrap();
-
-    let length = contents.len();
-
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
 }
