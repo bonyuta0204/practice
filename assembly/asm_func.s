7b854f86d170ebc6a95b9f47a4ef5fc2542fd732
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2024-03-07 21:52:55 +0900

    Add total asm func

--- assembly/asm_func.s
+++ assembly/asm_func.s
@@ -3,10 +3,21 @@
 ;------------
 bits 64
 section .text
-global sum
+global sum, total
 
 
 sum: ;int sum(int a, int b)
   MOV rax, rdi       ; Use rdi value
   ADD rax, rsi       ; Add rsi value
   RET
+
+total: ;int total(int a)
+  MOV rcx, 0      ; counter
+  MOV rax, 0      ; total
+loop:
+  INC rcx
+  ADD rax, rcx
+  CMP rcx, rdi ; compare counter and limit
+  JNE loop
+  RET
+
---
+++ assembly/total.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int total(int a);
+
+int main(){
+
+  int a = total(10);
+
+  printf("total is %d", a);
+
+}
