9674ea807fe8a3df91861a67a1df45131aa60a59
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-03-18 00:38:16 +0900

    add rust list

---
+++ leetcode/rust/src/bin/delete_duplicates.rs
@@ -0,0 +1,8 @@
+use leetcode::data_structures::list::ListNode;
+
+
+fn main(){
+    let nodes = ListNode::from_array(vec![1,2,3,4]);
+
+    println!("{:#?}",nodes);
+}
--- leetcode/rust/src/data_structures/list.rs
+++ leetcode/rust/src/data_structures/list.rs
@@ -8,4 +8,15 @@ impl ListNode {
     pub fn new(val: i32) -> Self {
         ListNode { next: None, val }
     }
+
+    pub fn from_array(vals: Vec<i32>) -> Option<Box<ListNode>> {
+        let mut head = None;
+        let mut tail = &mut head;
+        for val in vals {
+            let new_node = Box::new(ListNode::new(val));
+            *tail = Some(new_node);
+            tail = &mut tail.as_mut().unwrap().next;
+        }
+        head
+    }
 }
