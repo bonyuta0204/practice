2cf61a24e96b7d1d4d0d95d8908f1bab34a19df7
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-03-18 01:00:43 +0900

    add delete duplicates

--- leetcode/rust/src/bin/delete_duplicates.rs
+++ leetcode/rust/src/bin/delete_duplicates.rs
@@ -1,8 +1,31 @@
 use leetcode::data_structures::list::ListNode;
 
+fn main() {
+    let nodes = ListNode::from_array(vec![1, 1, 1, 4]);
 
-fn main(){
-    let nodes = ListNode::from_array(vec![1,2,3,4]);
+    let result = Solution::delete_duplicates(nodes);
 
-    println!("{:#?}",nodes);
+    println!("{:#?}", result);
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut head = head;
+        let mut current = &mut head;
+
+        while let Some(ref mut cur_node) = current {
+            while let Some(ref next_node) = cur_node.next {
+                if cur_node.val == next_node.val {
+                    cur_node.next = next_node.next.clone();
+                } else {
+                    break;
+                }
+            }
+            current = &mut cur_node.next;
+        }
+
+        head
+    }
 }
